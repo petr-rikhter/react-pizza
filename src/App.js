392ecd94f3239822af80d379f5871741e4a1c98c
Petr Rikhter <petr_rikhter@mail.ru>
2022-12-01 01:53:34 +0600

    added styles and first components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
